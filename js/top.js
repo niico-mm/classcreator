bee0e95fdb47846147fa74d5062d68f0689f3a52
松原恵 <mm.cosmos202@gmail.com>
2016-04-03 16:05:23 +0900

    ・ないものは反映しない

--- js/top.js
+++ js/top.js
@@ -13,13 +13,13 @@ function initSelect() {
 }
 
 function setHierSelect() {
-  $('#level-1').change(function(){
+  $('#level-1').on('change', function(){
     initSelect();
     var categoryName = $('#level-1 option:selected').data('cat');
     $('#level-2 [data-cat="' + categoryName + '"]').show();
 
     //rayoutのときにcmnの入力不可
-    if(categoryName == 'rayout') {
+    if(categoryName === 'rayout') {
       $('#level-3').attr('disabled', true).addClass('disabled');
     } else {
       $('#level-3').attr('disabled', false).removeClass('disabled');
@@ -28,34 +28,49 @@ function setHierSelect() {
 }
 
 function decisionInput () {
-  if ($('#level-2 option:not(.default)').is(':selected') && $('#level-4').val() != "") {
+  if ($('#level-2 option:not(.default)').is(':selected') && $('#level-4').val() !== "") {
     $('#js-trigger').attr('disabled', false).removeClass('disabled');
   } else {
     $('#js-trigger').attr('disabled', true).addClass('disabled');
   }
 }
+
 function createClass () {
-  $('#level-2').change(function(){
+  $('#level-2').on('change', function(){
     decisionInput();
   });
-  $('#level-4').keypress(function() {
-    decisionInput();
-  });
-  $('#level-4').keyup(function() {
+  $('#level-4').on('keyup', function() {
     decisionInput();
   });
 
-  $('#js-trigger').on("click", function(){
+  $('#js-trigger').on('click', function(){
     var lv2 = $('#level-2 option:selected').val();
     var lv3 = $('#level-3:checked').val();
     var lv4 = $('#level-4').val();
     var lv5 = $('#level-5').val();
 
+    // if (lv3) {
+    //   var primaryName = lv2 + '-' + lv3;
+    // } else {
+    //   var primaryName = lv2;
+    // }
+    //
+    // if (lv5) {
+    //   var secondaryName = lv4 + '-' + lv5;
+    // } else {
+    //   var secondaryName = lv4;
+    // }
+    //
+    // var createName = primaryName + '-' + secondaryName;
 
-
-    var createName = lv2 + '-' + lv3 + '-' + lv4 + '-' + lv5;
-
-    console.log(lv3);
+    var createName = lv2;
+    if (lv3) {
+      createName = createName + '-' + lv3;
+    }
+    createName = createName + '-' + lv4;
+    if (lv5) {
+      createName = createName + '-' + lv5;
+    }
 
     $('#js-create').text(createName);
   });

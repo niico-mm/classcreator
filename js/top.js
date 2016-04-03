7a393200b3dd70091caaafff496acdb57b8c1651
松原恵 <mm.cosmos202@gmail.com>
2016-04-03 13:36:57 +0900

    ・文字の連結表示

--- js/top.js
+++ js/top.js
@@ -19,6 +19,14 @@ function setHierSelect() {
 
 function createClass () {
   $('#js-trigger').on("click", function(){
-    $('#level-2 option').attr('value');
+    var lv2 = $('#level-2 option:selected').val();
+    var lv3 = $('#level-3:checked').val();
+    var lv4 = $('#level-4').val();
+    var lv5 = $('#level-5').val();
+    var createName = lv2 + '-' + lv3 + '-' + lv4 + '-' + lv5;
+
+    console.log(createName);
+
+    $('.txt-create').append(createName);
   });
 }
